ea2408aef77224e73ec3dd5a7bf21202c2a8fc80
Chris Firgens <cfirgens@duluthtrading.com>
2025-02-15 13:00:34 -0600

    added the fetchMovies and function to call movies from all sources in fetchMovies service, moved API urls to const folder

---
+++ src/consts/apiUrls.ts
@@ -0,0 +1,3 @@
+export const VHS_API = 'https://vhs.service.com';
+export const DVD_API = 'https://dvd.service.com';
+export const PROJECTOR_API = 'https://prjktr.service.com';
--- src/services/fetchMovies.ts
+++ src/services/fetchMovies.ts
@@ -1,12 +1,34 @@
 import axios from 'axios';
+import { ApiFilmSearch, Film } from '../types';
+import { VHS_API, DVD_API, PROJECTOR_API } from '../consts/apiUrls';
 
-const VHS_API = 'https://vhs.service.com';
-const DVD_API = 'https://dvd.service.com';
-const PROJECTOR_API = 'https://prjktr.service.com'
+export async function fetchMovies(apiUrl: string, params: ApiFilmSearch): Promise<Film[]> {
+    //Nice to have: Cache data
+    try {
+        const response = await axios.post<Film[]>(apiUrl,params);
+        return response.data;
+    } catch (err) {
+        console.error(`Error fetching data from ${apiUrl}:`, err)
+        return [];
+    }
+}
 
-// Fetch Movies returns []Films (api/params)
+export async function getMoviesFromAllSources(
+    excludeVHS: boolean,
+    excludeDVD: boolean,
+    excludeProjector: boolean,
+    params: ApiFilmSearch
+) : Promise<Film[]> {
+    const requests: Promise<Film[]>[] = [];
 
-// Fetch movies from all sources returns []Films (excludes/params)
+    if (!excludeVHS) requests.push(fetchMovies(VHS_API, params));
+    if (!excludeDVD) requests.push(fetchMovies(DVD_API, params));
+    if (!excludeProjector) requests.push(fetchMovies(PROJECTOR_API, params));
+
+    const results = await Promise.all(requests);
+    return results.flat();
+}
+
 
 //Deduplicate function
 
