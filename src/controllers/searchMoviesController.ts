cc46e4b77bd815fc9390bfb77bc6b718322bb8dc
Chris Firgens <cfirgens@duluthtrading.com>
2025-02-15 14:55:08 -0600

    finished the controller

---
+++ src/controllers/searchMoviesController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { getMoviesFromAllSources, paginateResults, deduplicateMovies } from '../services/fetchMovies';
+import { SearchRequest } from '../types';
+
+export async function searchMoviesController(req: Request, res: Response) {
+    try{
+        const searchParams: SearchRequest = req.body;
+
+        const movies = await getMoviesFromAllSources(
+            searchParams.excludeVHS || false,
+            searchParams.excludeDVD || false,
+            searchParams.excludeProjector || false,
+            searchParams
+        );
+    
+        const deduplicatedMovies = deduplicateMovies(movies);
+        const paginatedMovies = paginateResults(deduplicatedMovies, searchParams.currentPage, searchParams.pageSize);
+    
+        res.json({
+            currentPage: searchParams.currentPage,
+            pageSize: searchParams.pageSize,
+            totalResults: deduplicatedMovies.length,
+            results: paginatedMovies
+        });
+    } catch (err) {
+        console.error("Error processing movie search:", err);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+   
+}
